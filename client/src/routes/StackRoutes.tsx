9c8ed57d1052748236249ec12e046b3901acfc7e
ashish <a.asis.sigdel01@gmail.com>
2024-12-10 16:24:56 +0545

    pause

---
+++ client/src/routes/StackRoutes.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const StackRoutes = () => {
+  return (
+    <View>
+      <Text>StackRoutes</Text>
+    </View>
+  );
+};
+
+export default StackRoutes;
