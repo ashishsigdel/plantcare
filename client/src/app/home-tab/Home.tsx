b8b4b613112abbc8189e9b6b4eed02ba61383e58
ashish <a.asis.sigdel01@gmail.com>
2024-12-16 17:25:16 +0545

    home section UI completed

--- client/src/app/home-tab/Home.tsx
+++ client/src/app/home-tab/Home.tsx
@@ -1,16 +1,25 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
 import {HomeContent, HomeTop} from '../../components/home';
 
 const Home = () => {
   return (
-    <View>
-      <HomeTop />
-      <HomeContent />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <HomeTop />
+        <HomeContent />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollContainer: {
+    paddingBottom: 80,
+  },
+});
