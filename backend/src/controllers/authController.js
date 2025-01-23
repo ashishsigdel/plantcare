cf282248632c6e434e89f3e089a0fb13da8ae5a9
ashish <a.asis.sigdel01@gmail.com>
2025-01-23 17:04:30 +0545

    everything to start controller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,10 +1,28 @@
 import db from "../models/index.js";
+import ApiError from "../utils/apiError.js";
 import ApiResponse from "../utils/apiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
+const { User } = db;
+
 export const register = asyncHandler(async (req, res) => {
+  const { fullName, email, phone } = req.body;
+
+  if (!fullName) {
+    throw new ApiError({
+      status: 400,
+      message: "Name is required",
+    });
+  }
+  if (!email && !phone) {
+    throw new ApiError({
+      status: 400,
+      message: "Email or Phone is required",
+    });
+  }
   return new ApiResponse({
     status: 200,
     message: "User Registered!",
+    data: count,
   }).send(res);
 });
