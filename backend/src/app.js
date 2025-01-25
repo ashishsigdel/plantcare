5d2bcd48ff5da1a15dd9f8f73254c20f22683400
ashish <a.asis.sigdel01@gmail.com>
2025-01-25 10:47:37 +0545

    update cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,15 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
