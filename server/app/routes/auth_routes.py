4b88df78a1749498d632e6cf97b7f821105b25e6
bishal2059 <bishal.sap21@gmail.com>
2025-02-24 21:41:30 +0545

    model server done

--- server/app/routes/auth_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from app.controllers.auth_controller import register_user, login_user
-
-auth_route = Blueprint('auth_route', __name__)
-
-auth_route.add_url_rule('/api/register', 'register_user', register_user, methods=['POST'])
-auth_route.add_url_rule('/api/login', 'login_user', login_user, methods=['POST'])
