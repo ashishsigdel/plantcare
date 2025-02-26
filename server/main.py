400bfe0626016be9c502aceb8b94f81f90c3cba2
bishal2059 <bishal.sap21@gmail.com>
2025-02-26 21:40:14 +0545

    server fixes

--- server/main.py
+++ server/main.py
@@ -57,4 +57,4 @@ def predict():
     # return send_file(img_io, mimetype='image/jpeg')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000)
